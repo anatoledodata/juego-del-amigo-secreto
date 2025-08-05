3774aaaa54527b84e25b6e426511902912c99e7e
Ana Paula Toledo <ana.toledo.data@gmail.com>
2025-08-05 20:50:21 -0300

    Crea función agregarAmigo

--- challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,24 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigos = [];
+
+function agregarAmigo() {
+    let amigos = document.getElementById('amigo').value.trim().toLowerCase();
+    console.log("Nombre ingresado:", amigos);
+
+    if (amigos === ""){
+        alert ('Por favor, inserte un nombre');
+    } else {
+        if (listaDeAmigos.includes(amigos)){
+            console.log("Ya existe:", amigos);
+            alert('Este nombre ya ha sido ingresado');
+        } else {
+            listaDeAmigos.push(amigos);
+            console.log("Lista actualizada:", listaDeAmigos);
+        }
+        limpiarCaja();
+    }
+}
+
+
+function limpiarCaja () {
+    document.getElementById('amigo').value = ""; 
+}
